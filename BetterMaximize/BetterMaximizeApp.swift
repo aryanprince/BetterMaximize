58033b9d28e4e2f3c04c299899fd7989aeab605b
Aryan Prince <aryanprincesanty@icloud.com>
2024-01-21 02:26:51 +0000

    Initial Commit

---
+++ BetterMaximize/BetterMaximizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterMaximizeApp.swift
+//  BetterMaximize
+//
+//  Created by Aryan Prince on 1/21/24.
+//
+
+import SwiftUI
+
+@main
+struct BetterMaximizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

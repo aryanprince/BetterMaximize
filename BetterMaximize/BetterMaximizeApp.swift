f313114aa922cf23f3c8580124d66c52e31fd5b0
Aryan Prince <aryanprincesanty@icloud.com>
2024-01-21 03:32:10 +0000

    ✨ Built simple UI to toggle feature and quit app

--- BetterMaximize/BetterMaximizeApp.swift
+++ BetterMaximize/BetterMaximizeApp.swift
@@ -9,9 +9,35 @@ import SwiftUI
 
 @main
 struct BetterMaximizeApp: App {
+    @State var currentStatus: Bool = false
+    
     var body: some Scene {
-        WindowGroup {
-            ContentView()
+        MenuBarExtra("Some title key", systemImage: "plus.app") {
+            Text("Current status:")
+            if (currentStatus) {
+                Text("Enabled")
+            }
+            else {
+                Text("Disabled")
+            }
+            
+            Divider()
+            
+            Button("Enable") {
+                currentStatus = true
+            }
+            .keyboardShortcut("e")
+            
+            Button("Disable") {
+                currentStatus = false
+            }
+            .keyboardShortcut("d")
+            
+            Divider()
+            
+            Button("Quit") {
+                NSApplication.shared.terminate(nil)
+            }.keyboardShortcut("q")
         }
     }
 }
